6fb2c0075b646873550297fbdc13f70a22f3fae6
yagoubali <yagoubali@gmail.com>
2021-11-25 10:55:08 -0300

    handling missing of fusion file

--- app/pipeline_scripts/pascal.sh
+++ app/pipeline_scripts/pascal.sh
@@ -131,30 +131,36 @@ else
 fi
 ### Filter genescores results
 genescores_file=$(ls ${outdir}| grep ${genescoring}.genescores*);
-genescores_output=$(echo ${genescores_file}| sed -e 's/.txt/_filtered.txt/');
-sed -i 's/,/./g' ${outdir}/${genescores_file};
-awk -v pvalue=$pvalue_cutoff '{if(NR==1) print $0; if (($8)<=pvalue) print $0}' ${outdir}/${genescores_file} > ${outdir}/${genescores_output}
-#Rscript --vanilla ${bin_dir}/filterGeneScoresFiles.R ${outdir}/${genescores_file} ${outdir}/${genescores_output} ${pvalue_cutoff}
-### Filter out pathways results based on pvalue cutoff
+if [[ -f "${outdir}/${genescores_file}" ]]; then
+    genescores_output=$(echo ${genescores_file}| sed -e 's/.txt/_filtered.txt/');
+    sed -i 's/,/./g' ${outdir}/${genescores_file};
+    awk -v pvalue=$pvalue_cutoff '{if(NR==1) print $0; if (($8)<=pvalue) print $0}' ${outdir}/${genescores_file} > ${outdir}/${genescores_output}
+else 
+    touch ${outdir}/no_genescores_output_filtered.txt;
+fi
+
+
+
 if [[ "$runpathway" == "on" ]]; then
   ## prepare Output files name.
   fusion_file=$(ls ${outdir}| grep ${genescoring}.fusion.genescores*);
   if [[ -f "${outdir}/${fusion_file}" ]]; then
-  fusion_output=$(echo ${fusion_file}| sed -e 's/.txt/_filtered.txt/');
-  sed -i 's/,/./g' ${outdir}/${fusion_file};
-  awk -v pvalue=$pvalue_cutoff '{if(NR==1) print $0; if (($8)<=pvalue) print $0}' ${outdir}/${fusion_file} > ${outdir}/${fusion_output}
+      fusion_output=$(echo ${fusion_file}| sed -e 's/.txt/_filtered.txt/');
+      sed -i 's/,/./g' ${outdir}/${fusion_file};
+      awk -v pvalue=$pvalue_cutoff '{if(NR==1) print $0; if (($8)<=pvalue) print $0}' ${outdir}/${fusion_file} > ${outdir}/${fusion_output}
   else 
-  touch ${outdir}/no_fusion_output_filtered.txt;
+      touch ${outdir}/no_fusion_output_filtered.txt;
   fi
-  pathway_file=$(ls ${outdir}| grep PathwaySet)
-  pathway_output=$(echo ${pathway_file}| sed -e 's/.txt/_filtered.txt/')
   
-  #Rscript --vanilla ${bin_dir}/filterGeneScoresFiles.R ${outdir}/${fusion_file} ${outdir}/${fusion_output} ${pvalue_cutoff}
+  pathway_file=$(ls ${outdir}| grep PathwaySet)
+  if [[ -f "${outdir}/${pathway_file}" ]]; then
+      pathway_output=$(echo ${pathway_file}| sed -e 's/.txt/_filtered.txt/')
+      sed -i 's/,/./g' ${outdir}/${pathway_file}
+      awk -v pvalue=$pvalue_cutoff '{if(NR==1) print $0; if (($2)<=pvalue) print $0}' ${outdir}/${pathway_file} > ${outdir}/${pathway_output}
+  else 
+     touch ${outdir}/no_pathway_output_filtered.txt;
+  fi
 
-  ## replace comma with period for math comparison
-  sed -i 's/,/./g' ${outdir}/${pathway_file}
-  awk -v pvalue=$pvalue_cutoff '{if(NR==1) print $0; if (($2)<=pvalue) print $0}' ${outdir}/${pathway_file} > ${outdir}/${pathway_output}
-  #Rscript --vanilla ${bin_dir}/filterPathwayFile.R ${outdir}/${pathway_file} ${outdir}/${pathway_output} ${pvalue_cutoff}
 
 fi
 
